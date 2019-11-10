7afe6da0e5be48cb3a25cf6ff81a16dec28b9d51
ARISTIDE15 <57256650+ARISTIDE15@users.noreply.github.com>
2019-11-10 23:52:47 +0100

    Update Contrôleur.py
    Doumninga klamte Aristide#18B598FS

--- Contrôleur.py
+++ Contrôleur.py
@@ -1,1 +1,9 @@
-
+Class Controleur(PersonneBank):
+     solde=int(input"solde"))
+     montant=int(intput("montant"))
+     if  solde<=0:
+     else:
+         if solde<montant:
+             print(pas de transaction")
+         else:
+             print("transaction=ok")
